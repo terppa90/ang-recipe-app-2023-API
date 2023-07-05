e00d83094341e725062a776fa86258e3a53077fc
terppa90 <teromese@hotmail.com>
2023-07-05 16:21:46 +0300

    v 1.0

--- controllers/recipecontroller.js
+++ controllers/recipecontroller.js
@@ -36,21 +36,31 @@ const RecipeController = {
   },
 
   // Päivitetään resepti (Ei vielä käytössä clientissa)
+  // updateRecipe: async (req, res) => {
+  //   try {
+  //     const { id } = req.params;
+  //     const recipe = await Recipe.findByIdAndUpdate(id, req.body);
+  //     if (!recipe) {
+  //       return res
+  //         .status(404)
+  //         .json({ message: `Cannot find any product with ID ${id}` });
+  //     }
+  //     const updatedRecipe = await Recipe.findById(id);
+  //     res.status(200).json(updatedRecipe);
+  //   } catch (error) {
+  //     console.log(error.message);
+  //     res.status(500).json({ message: error.message });
+  //   }
+  // },
+
   updateRecipe: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const recipe = await Recipe.findByIdAndUpdate(id, req.body);
-      if (!recipe) {
-        return res
-          .status(404)
-          .json({ message: `Cannot find any product with ID ${id}` });
-      }
-      const updatedRecipe = await Recipe.findById(id);
-      res.status(200).json(updatedRecipe);
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ message: error.message });
-    }
+    await Recipe.findOneAndUpdate({ id: req.params.id }, req.body)
+      .then((result) => {
+        res.status(200).json({ result });
+      })
+      .catch((err) => {
+        return res.status(500).json({ message: err.message });
+      });
   },
 };
 
