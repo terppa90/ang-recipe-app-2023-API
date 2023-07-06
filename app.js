dd8ee212d98e8a0aa619ed901f750a747f1e4172
terppa90 <teromese@hotmail.com>
2023-07-06 13:09:08 +0300

    v 1.0

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Import routes
 const recipesRoute = require('./routes/recipes');
